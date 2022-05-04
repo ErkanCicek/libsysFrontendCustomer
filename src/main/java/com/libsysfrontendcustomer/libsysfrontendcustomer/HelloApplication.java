010227cc55bd467061f87a542c3cbbee7da519c7
bryan <bryan.bautista830@gmail.com>
2022-05-04 16:10:59 +0200

    Erkan, Lahoud och Bryan

--- src/main/java/com/libsysfrontendcustomer/libsysfrontendcustomer/HelloApplication.java
+++ src/main/java/com/libsysfrontendcustomer/libsysfrontendcustomer/HelloApplication.java
@@ -2,6 +2,7 @@ package com.libsysfrontendcustomer.libsysfrontendcustomer;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -10,10 +11,12 @@ import java.io.IOException;
 public class HelloApplication extends Application {
 	@Override
 	public void start(Stage stage) throws IOException {
-		FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-		Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-		stage.setTitle("Hello!");
+		FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("StartPage.fxml"));
+		Scene scene = new Scene(fxmlLoader.load());
+		stage.setTitle("Kungliga Stadsbiblioteket");
 		stage.setScene(scene);
+		//stage.setFullScreen(true);
+		stage.setMaximized(true);
 		stage.show();
 	}
 
