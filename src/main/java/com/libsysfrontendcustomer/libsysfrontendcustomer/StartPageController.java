42cd339ce4654b61c2ab5cfd34aba372a3d589ab
bryan <bryan.bautista830@gmail.com>
2022-05-26 16:49:05 +0200

    Erkan, Lahoud och Bryan - Mina sidor är klart! TableView för både Lånade böcker samt reserverade rum

--- src/main/java/com/libsysfrontendcustomer/libsysfrontendcustomer/StartPageController.java
+++ src/main/java/com/libsysfrontendcustomer/libsysfrontendcustomer/StartPageController.java
@@ -2,27 +2,28 @@ package com.libsysfrontendcustomer.libsysfrontendcustomer;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+import com.libsysfrontendcustomer.libsysfrontendcustomer.models.BorrowedBooks;
+import com.libsysfrontendcustomer.libsysfrontendcustomer.models.RoomResDD;
+import com.libsysfrontendcustomer.libsysfrontendcustomer.models.TVBorrowedBooks;
+import com.libsysfrontendcustomer.libsysfrontendcustomer.models.TVReservedRooms;
 import com.libsysfrontendcustomer.libsysfrontendcustomer.models.util.DateHelper;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.lang.reflect.Type;
-import java.net.URL;
 import java.util.Map;
 import java.util.Objects;
-import java.util.ResourceBundle;
 
-public class StartPageController implements Initializable {
+public class StartPageController {
     Book book;
 
     public TextField inputField;
@@ -66,6 +67,36 @@ public class StartPageController implements Initializable {
     @FXML
     public TextArea TAMinaSidor;
 
+    @FXML
+    public TableView<TVBorrowedBooks> TVLB;
+    @FXML
+    public TableColumn<TVBorrowedBooks, Integer> CID;
+    @FXML
+    public TableColumn<TVBorrowedBooks, String> CÅB;
+    @FXML
+    public TableColumn<TVBorrowedBooks, String> CISBN;
+    @FXML
+    public TableColumn<TVBorrowedBooks, String> CPersonnummer;
+    @FXML
+    public TableColumn<TVBorrowedBooks, Integer> CBID;
+
+    public ObservableList<TVBorrowedBooks> observableList = FXCollections.observableArrayList();
+
+    @FXML
+    public TableView<TVReservedRooms> TVRR;
+    @FXML
+    public TableColumn<TVReservedRooms, String> CDatum;
+    @FXML
+    public TableColumn<TVReservedRooms, String> CTid;
+    @FXML
+    public TableColumn<TVReservedRooms, Integer> CLID;
+    @FXML
+    public TableColumn<TVReservedRooms, Integer> CRID;
+
+    public ObservableList<TVReservedRooms> observableList1 = FXCollections.observableArrayList();
+
+    @FXML
+    private Button btnValidate;
 
     public void handleBtnSBok() throws Exception{
 
@@ -126,17 +157,43 @@ public class StartPageController implements Initializable {
         boolean isVerified = (boolean) verifyLoginOutput.get("booleanvalue");
         if (isVerified){
             LWarningPN.setText("Konfirmerad!");
-            TAMinaSidor.setText(connectionManager.sendGetRequest("borrowedbooks/get/getAllBorrowedBooksBySSN?value=" + Personnummer));
+
+            BorrowedBooks[] borrowedBooks = new Gson().fromJson(connectionManager.sendGetRequest("borrowedbooks/get/getAllBorrowedBooksBySSN?value=" + Personnummer), BorrowedBooks[].class);
+            for (BorrowedBooks borrowedBooks1 : borrowedBooks){
+                observableList.add(new TVBorrowedBooks(
+                        borrowedBooks1.getBorrowedBooksID(), borrowedBooks1.getReturnDate(), borrowedBooks1.getBookISBN(), borrowedBooks1.getBorrowerSSN(), borrowedBooks1.getBookId())
+                );
+
+                CID.setCellValueFactory(new PropertyValueFactory<>("borrowedBooksID"));
+                CÅB.setCellValueFactory(new PropertyValueFactory<>("returnDate"));
+                CISBN.setCellValueFactory(new PropertyValueFactory<>("bookISBN"));
+                CPersonnummer.setCellValueFactory(new PropertyValueFactory<>("borrowerSSN"));
+                CBID.setCellValueFactory(new PropertyValueFactory<>("bookId"));
+
+                TVLB.setItems(observableList);
+            }
+
+            RoomResDD[] roomResDDS = new Gson().fromJson(connectionManager.sendGetRequest("roomRes/get/getAllReservationsBySSN?value=" + Personnummer), RoomResDD[].class);
+            for (RoomResDD roomResDD : roomResDDS){
+                observableList1.add(new TVReservedRooms(
+                        roomResDD.getDateValue(), roomResDD.getTime() + ": 00 - " + (roomResDD.getRoomId() + 2) + " : 00" , roomResDD.getBorrowerId(), roomResDD.getRoomId())
+                );
+                CDatum.setCellValueFactory(new PropertyValueFactory<>("dateValue"));
+                CTid.setCellValueFactory(new PropertyValueFactory<>("time"));
+                CLID.setCellValueFactory(new PropertyValueFactory<>("borrowerId"));
+                CRID.setCellValueFactory(new PropertyValueFactory<>("roomId"));
+
+                TVRR.setItems(observableList1);
+            }
+
+            btnValidate.setVisible(false);
+            btnTillbaka4.setText("Logga ut!");
+
         } else if (TFPN.getText().isBlank() || TFPassword.getText().isBlank()){
             LWarningPN.setText("Vänligen skriv in er personnummer samt lösenord först för att kontollera!");
         } else {
             LWarningPN.setText("Finns ingen person med denna personnummer eller lösenord!");
         }
-    }
-
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-
     }
 
     public void lånaBokBtnOnAction(ActionEvent event) {
